1b29256e731e9f30c0990d4cec8511299534b74b
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-19 07:47:08 +0500

    Python strings & Variables

---
+++ string.py
@@ -0,0 +1,50 @@
+# print('hello , world')
+# print(type("hello , world"))
+
+# print("786$")
+# print(type("786$"))
+
+
+# print('hello \' world')
+
+# print("hello \" world")
+
+
+
+# import keyword
+# print(keyword.kwlist)
+
+
+
+# Variable
+
+# name = " HELLO , Qamar"
+
+# print(name)
+
+
+# Rules for Variable Names
+
+# name= 'Hello qamar'
+# _name="hello"
+# # 6='hello'
+# print='hello'
+# print(print)
+
+
+# import keyword
+# print(keyword.kwlist)
+
+# my_name_is ='qamar'
+# print(my_name_is)
+ 
+# my-name='hello'
+# print(my-name)
+
+# my_name='hello '
+
+
+# my_name ='world'
+# print(my_name)
+
+
