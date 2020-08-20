233f3a8acccb77efc4b9d1aa876b45f65891d500
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-20 09:16:34 +0500

    String Methods & String Formatting
    In this tutorial we will discuss  what are  string methods and how they perform functionality on Strings , Concatination and String Formatting , how we can use variables inside our strings and much more .
    All the stuff is in  Urdu and you can learn in easy manners   .
    Stay Safe ,  Stay Connected
    
    To Stay Updated about our plans & Videos like our Facebook page
    #Facebook Page :   https://web.facebook.com/codingwithqamar/?modal=admin_todo_tour
    #Linkedin Profile :  https://www.linkedin.com/in/qamar-ashfaq-0585a81a1/

---
+++ string_methods.py
@@ -0,0 +1,45 @@
+
+color='red green'
+
+fruit= 'Apple'
+# print(color.lower())
+# print(color.upper())
+# print(color.capitalize())
+
+# print(len('hello  world'))
+
+# result=color.count('e')
+# print(result)
+
+# replacement=color.replace('red','yellow')
+# print(replacement)
+
+
+# name = 'hello world'
+
+# print(name[3])
+
+# print(name.index('w'))
+
+# print(name.find('z'))
+
+# print('hello' , 'world')
+# print('hello ' + 'world')
+
+
+# print('my favourite color is : ' , color)
+# print('my favourite color is : ' + color)
+
+# print('my favourite color is {} , and fruit is {} '.format(fruit,color) )
+
+
+
+print(f'my favourite color is {color} , and fruit is {fruit} ')
+
+
+
+
+
+
+
+
