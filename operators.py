384c6f4cab7f161165e6577d7febe76cbf8634fe
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-21 09:23:21 +0500

    Python Operators

---
+++ operators.py
@@ -0,0 +1,103 @@
+                                     # Operators in Python
+
+'''Python Operator falls into 7 categories:
+
+i)Python Arithmetic Operator
+ii)Python Relational Operator
+iii)Python Assignment Operator 
+iv)Python Logical Operator
+v)Python Membership Operator  (in , not in)
+vi)Python Identity Operator  (is , is not)
+vii)Python Bitwise Operator     '''
+
+
+
+# Python Arithmetic Operator
+
+# a. Addition(+)
+'''Adds the values on either side of the operator.'''
+3+4
+
+# b. Subtraction(-) 
+'''Subtracts the value on the right from the one on the left.'''
+3+4
+
+# Multiplication(*)
+'''Multiplies the values on either side of the operator.'''
+3*4
+
+# d. Division(/)
+'''Divides the value on the left by the one on the right.
+ Notice that division results in a floating-point value.'''
+
+# e. Exponentiation(**)
+'''Raises the first number to the power of the second.'''
+3**4
+
+# f. Floor Division(//)
+
+'''Divides and returns the integer value of the quotient.
+ It dumps the digits after the decimal.'''
+10//3
+#  output : 3
+
+# g. Modulus(%)
+
+'''Divides and returns the value of the remainder.'''
+print(4%3)
+
+# output : 1
+
+# f. paranthesis()
+
+# Python Relational Operator
+
+'''Relational Python Operator carries out the comparison between operands.
+ They tell us whether an operand is greater than the other, lesser, equal, 
+ or a combination of those.'''
+
+#  a. Less than(<)
+# b. Greater than(>)
+# c. Less than or equal to(<=)
+# d. Greater than or equal to(>=)
+# e. Equal to(= =)
+# f. Not equal to(!=)
+
+
+    # Python Assignment Operator
+
+# a. Assign(=)
+x=2
+
+# b. Add and Assign(+=)
+x +=2
+
+# c. Subtract and Assign(-=)
+
+x -=2
+# ..........
+
+    # Python Logical Operator
+
+# a. and
+
+'''If the conditions on both the sides of the operator are true,
+ then the expression as a whole is true.'''
+
+a=7>7 and 2>-1
+print(a)
+
+# output : False
+
+# b. or
+
+'''The expression is false only if both the statements around the operator are false.
+ Otherwise, it is true.'''
+
+a=7>7 or 2>-1
+print(a)
+
+# output : True
+
+
+
