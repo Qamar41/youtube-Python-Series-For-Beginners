555ed575b5383459f76b25187405a83b5ad67ccb
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-22 01:08:37 +0500

    conditional Statements

---
+++ conditions.py
@@ -0,0 +1,103 @@
+            # Conditional Statements
+                
+                # i)   if 
+                # ii)  elif 
+                # iii) else
+    
+    
+    
+    
+    
+    
+                    # Comparisons :
+        # Equal:            ==
+        # Not Equal:        !=
+        # Greater Than:     >
+        # Less Than:        <
+        # Greater or Equal: >=
+        # Less or Equal:    <=
+        # Object Identity:  is
+
+
+        # Code starts   
+
+        # if condition :
+        #     print('st')
+
+
+
+# marks=45
+
+# if marks >=50:
+#     print('You are passed .')
+
+# else:
+#     print('YOu are fail .')
+
+
+
+
+
+
+
+# marks = int(input('Enter your marks : '))
+
+
+# if marks >=80:
+#     print('your grade is A, and congratulations :')
+
+# elif marks >=70:
+#     print('your grade is B , Welldone')
+
+# elif marks >=60:
+#     print('your grade is c , Good')
+
+# elif marks >=50:
+#     print('you are only pass : ')
+# else:
+#     print('you are fail , try again')
+
+
+
+# user_name= 'Sanan'
+# loged_in=True
+
+
+# if user_name=='Ahmad' and loged_in:
+#     print(f'Welcome to admin page {user_name}')
+# else:
+#     print('login first to go admin page')
+
+# user_name= 'Sanan'
+# loged_in=True
+
+
+# if  user_name=='Ahmad' or loged_in:
+#     print(f'Welcome to admin page {user_name}')
+# else:
+#     print('login first to go admin page')
+
+
+
+
+
+
+# country=input('Enter your Coutry : ')
+
+# # 1 for Pakistan and 0 for other's
+
+# if country == '1':
+#     print('Welcome Out Pakistani Brother .')
+#     city=input('Enter your City :')
+#     if city=='Lahore':
+#         print('Áre you really from Lahore .')
+#     else:
+#         print('you are not from Lahore ')
+# else:
+#     print('Only Pakistani Nationality is allowed')
+
+a=[1,2,3]
+b=[2,4]
+a.extend(b)
+print(a)
+
