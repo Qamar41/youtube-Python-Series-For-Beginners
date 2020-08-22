26699f71bbb87fa8a59ff1b6674c3add351eca63
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-22 23:03:26 +0500

    Create tuple&set.py

---
+++ tuple&set.py
@@ -0,0 +1,20 @@
+                                         # Tuples & Sets
+
+
+
+# names = ('Qamar', 21, True)
+# # print(names.remove('Qamar'))
+
+# print('qamar' in names)
+
+
+                            # Sets
+
+
+subjects={'Physics', 'Chemistry','Math','Computer Science','Physics'}
+# subjects_2={'Urdu','Englis','Math','Chemistry'}
+# # print(type(subject))
+
+# print(subjects.intersection(subjects_2))
+# print(subjects.difference(subjects_2))
+print(subjects)
