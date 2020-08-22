62d7387efb1bd2f77b1d2f96746baf29c36a83ae
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-22 14:05:04 +0500

    Lists & List Methods

---
+++ list.py
@@ -0,0 +1,55 @@
+                # Python List
+
+
+# names = ['Sanan', 'Ahmad', 'Iran', 5 ,7 ,7.6]
+# print(type(names))
+
+# print(names[2:])
+
+# print(len(names))
+
+# names.append('Hammad')
+
+# print(names)
+# names.insert(1,'Mustafa')
+
+# names.extend([6,7,8])
+# names.append([6,7,8])
+# print(len(names))
+
+names = ['Sanan', 'Ahmad', 'Irfan', 5 ,7 ,7.6]
+
+# names.remove('Irfan')
+
+# names.pop(1)
+
+# del names[3]
+
+numbers =[3,2,6,9,5]
+
+# names.reverse()
+
+# numbers.sort()
+
+
+# new_list= sorted(numbers)
+# print(new_list)
+
+# print(numbers)
+# print(numbers)
+
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+# print(numbers.index(12))
+
+
+# Membership operator in List (in , not in )
+
+print(8 in numbers)
+
+
+# output will always be in Boolean (True or False )
+# if the value you find will be in the list then output will be True 
+# and if the value not found in the list then it will be False
