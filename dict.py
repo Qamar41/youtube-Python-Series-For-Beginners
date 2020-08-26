0994e0892915eb072d92f10aef353abff00fdf26
Qamar Ashfaq <qamarashfaq41@gmail.com>
2020-08-26 17:26:07 +0500

    dictionary

---
+++ dict.py
@@ -0,0 +1,37 @@
+                                  #  Python Dictionaries 
+
+
+dictt={'name':'Qamar','age':23,'Single':True,'hobbies':['programming','gardening']}
+
+# print(dictt['agee'])
+
+
+# print(dictt.get('hobbiess','key not found'))
+
+# dictt['phone']=4373737
+
+# dictt.update({'mobile':'Samsung','age':25})
+
+
+# del dictt['age']
+
+# dictt.pop('name')
+
+# print(dictt['hobbies'][1])
+
+
+
+# print(dictt.keys())
+# print(dictt.values())
+# print(dictt.items())
+
+
+
+
+
+# print(type(dictt))
+# print(len(dictt))
+
+
+for key , value in dictt.items():
+    print(key , value)
---
+++ loops.py
@@ -0,0 +1,34 @@
+                    # python Loops
+
+# for i in range(1,10+1):
+#     print(i)
+
+
+# name = 'Qamar'
+
+# # for n in name:
+# #     print(n)
+
+# for n in range(15+1):
+#     print(n,name)
+
+
+
+# colors=['red','green','gray']
+# fruits=['Banana','orange']
+
+# for i in colors:
+#     for j in fruits:
+#         print(i,j)
+
+
+
+# 5*1=5
+
+# table = int(input('Which table you want ? :'))
+# terminate = int(input('where you want to terminate the table ? :'))
+
+
+# for i  in range(1,terminate+1):
+#     print(f'{table} *  {i} =' , table*i)
+
